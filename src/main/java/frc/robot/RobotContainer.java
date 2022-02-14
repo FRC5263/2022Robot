4eb79b11495f5b2f7d69c30ffb487e3ba804ece2
guyish1 <piercewlangford@outlook.com>
2022-02-14 16:52:21 -0500

    some minor modifications

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.DifferentialDriveTrainControl;
 import frc.robot.subsystems.DriveTrainSubsystem;
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
@@ -51,10 +52,13 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    final XboxController m_controller = new XboxController(0);
+    
+  }
 
   public Command getTeleOpCommad() {
-
+    
     return m_teleOp;
   }
 
