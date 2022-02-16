1e7dccd3e48673342b79991243661aa97d813750
guyish1 <piercewlangford@outlook.com>
2022-02-16 08:54:56 -0500

    added the PowerShooter function and comments

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -9,17 +9,25 @@ import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShooterSubsystem extends SubsystemBase {
-
+  //variable deleration 
   MotorController rightShooterMotor;
   MotorController leftShooterMotor;
   MotorControllerGroup shooter;
 
   /** Creates a new ShooterSubsystem. */
   public ShooterSubsystem(MotorController rightShooterMotor, MotorController leftShooterMotor) {
+    //variable declerations and assignments
+    System.out.print("Creating new shooter subsystem\n");
     this.rightShooterMotor = rightShooterMotor;
     this.leftShooterMotor = leftShooterMotor;
     shooter = new MotorControllerGroup(rightShooterMotor, leftShooterMotor);
     this.shooter = shooter;
+    System.out.print("new shooter subsystem created");
+  }
+
+  /**sets the power of the motors to power*/
+  public void PowerShooter(float power) {
+    shooter.set(power);
   }
 
   @Override
