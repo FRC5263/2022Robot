1b12dc7465d058e9c4d9e1b01ad15e33897c1ca5
guyish1 <piercewlangford@outlook.com>
2022-02-28 21:54:18 -0500

    created a file for variable declerations and such

---
+++ src/main/java/frc/robot/RobotVariables.java
@@ -0,0 +1,28 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.CounterBase.EncodingType;
+
+/** Add your docs here. */
+public class RobotVariables extends Robot{
+    //varibable declerations
+    //wheel info
+    public double backWheelDiameter;
+    public double frontWheelDiameter;
+    public double backWheelCircumfrence;
+    public double frontWheelCircumfrence;
+
+    //encoders for the wheels
+    public Encoder frontRightEncoder = new Encoder(0, 1, false, Encoder.EncodingType.k2X);
+    public Encoder frontLeftEncoder = new Encoder(2, 3, false, EncodingType.k2X);
+    public Encoder rearRightEncoder = new Encoder(4, 5, false, EncodingType.k2X);
+    public Encoder rearLeftEncoder = new Encoder(6, 7, false, EncodingType.k2X);
+
+    public RobotVariables() {}
+
+
+}
