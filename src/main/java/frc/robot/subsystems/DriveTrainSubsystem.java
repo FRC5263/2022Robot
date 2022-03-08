5238826dd4c9c02a2bfd73a777d08f93c8cb57cb
guyish1 <piercewlangford@outlook.com>
2022-03-08 12:30:55 -0500

    added drivetrain encoder stuff and added some docs

--- src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -23,9 +23,16 @@ public class DriveTrainSubsystem extends SubsystemBase {
   Encoder frontLeftEncoder;
   Encoder rearRightEncoder;
   Encoder rearLeftEncoder;
+  Encoder[] encoders = {frontRightEncoder, frontLeftEncoder, rearRightEncoder, rearLeftEncoder};
   DifferentialDrive differentialDrivetrain;
 
-  
+  /**
+   * creates a 4 motor differentail drivetrain
+   * @param frontRightMotor the front right motor
+   * @param frontLeftMotor the front left motor
+   * @param rearRightMotor the rear right motor
+   * @param rearLeftMotor the rear left motor
+   */
   public DriveTrainSubsystem(MotorController frontRightMotor, MotorController frontLeftMotor, MotorController rearRightMotor, MotorController rearLeftMotor) {
     //makes variables for speedcontollers
     System.out.print("creating new drivetrain subsystem\n");
@@ -40,6 +47,11 @@ public class DriveTrainSubsystem extends SubsystemBase {
     System.out.print("new drivetrain subsystem created\n");
   }
 
+  /**
+   * creates a 2 motor differentail drivetrain
+   * @param rightMotor the right motor
+   * @param leftMotor the left motor
+   */
   public DriveTrainSubsystem(MotorController rightMotor, MotorController leftMotor) {
     //makes variables for speedcontollers
     System.out.print("creating new drivetrain subsystem\n");
@@ -50,11 +62,20 @@ public class DriveTrainSubsystem extends SubsystemBase {
     System.out.print("new drivetrain subsystem created\n");
   }
 
+  /**
+   * creates a four encoders for a four motor drivetrain 
+   * @param frontRightEncoder
+   * @param frontLeftEncoder
+   * @param rearRightEncoder
+   * @param rearLeftEncoder
+   */
   public DriveTrainSubsystem(Encoder frontRightEncoder, Encoder frontLeftEncoder, Encoder rearRightEncoder, Encoder rearLeftEncoder) {
+    System.out.print("creating new drivetrain Encoders\n");
     this.frontRightEncoder = frontRightEncoder;
     this.frontLeftEncoder = frontLeftEncoder;
     this.rearLeftEncoder = rearLeftEncoder;
     this.rearRightEncoder = rearRightEncoder;
+    System.out.print("new drivetrain encoders created\n");
   }
 
   /**use this function to make a Differential Drive Train move with a tank drive control scheme. Left and Right will move independantly of each other */
@@ -65,6 +86,20 @@ public class DriveTrainSubsystem extends SubsystemBase {
   /**use this funtion to make Differntail Drive Train move with a curverture drive control scheme. Left and Right work together*/
   public void DriveDifferentialCurvature(double xSpeed, double zRotation) {
     differentialDrivetrain.curvatureDrive(xSpeed, zRotation, true);
+  }
+
+  /**
+   * 
+   * @param encoder input starts from 0. Order is right-left, front-rear so frontRight would be 2
+   * @return returns the 
+   */
+  public int getEncoder(int encoder) {
+    if(encoders[encoder] != null) {
+      return encoders[encoder].get();
+    } else {
+      System.out.print(encoders[encoder] + " is not pluged in, cannot get encoder count");
+      return -1;
+    }
   }
 
 
