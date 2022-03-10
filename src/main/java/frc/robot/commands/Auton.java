7dc26cf31e78ef744af96aec3330b2833f868779
guyish1 <piercewlangford@outlook.com>
2022-03-10 16:29:11 -0500

    auton stuff

--- src/main/java/frc/robot/commands/Auton.java
+++ src/main/java/frc/robot/commands/Auton.java
@@ -4,25 +4,42 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveTrainSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
+
+import com.kauailabs.navx.frc.*;
 
 public class Auton extends CommandBase {
-  /** Creates a new Auton. */
-  public Auton() {
-    // Use addRequirements() here to declare subsystem dependencies.
+  private final AHRS gyro = new AHRS(SPI.Port.kMXP);
+  private DriveTrainSubsystem drivetrain;
+  private DriveTrainSubsystem drivetrainEncoders;
+
+  /** Creates a new Auton command. */
+  public Auton(DriveTrainSubsystem drivetrain, DriveTrainSubsystem drivetrainEncoders, ShooterSubsystem shooter) {
+    System.out.print("Creating new auton command\n");
+    this.drivetrain = drivetrain;
+    this.drivetrainEncoders = drivetrainEncoders;
+    System.out.print("new auton commad created\n");
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    System.out.print("auton command initialized\n");
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    System.out.print("auton command ended\n");
+  }
 
   // Returns true when the command should end.
   @Override
